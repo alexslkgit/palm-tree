8718b443a1a2c0516c468319c131db507dae820e
alexslkgit <alexslkmain@gmail.com>
2024-03-13 12:03:25 +0000

    Project is created

---
+++ Palm tree/Palm tree/Palm_treeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Palm_treeApp.swift
+//  Palm tree
+//
+//  Created by Slobodianiuk Oleksandr on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Palm_treeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
